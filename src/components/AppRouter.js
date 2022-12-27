c4c583216e0ce1e98ba84e44b8c92fca8a27cd84
ginameeeBV <jc.lee@birdview.kr>
2022-12-27 16:06:05 +0900

    3.6 Log Out

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,16 +1,28 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
           </>
         ) : (
           <Route exact path="/">
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,19 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+
+  const handleClickLogOut = async () => {
+    await authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={handleClickLogOut}>Log Out</button>
+    </>
+  );
+};
 export default Profile;
