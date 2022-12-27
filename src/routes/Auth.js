c948d347d726ed729c5c0a11f56aee8957d7ab5a
ginameeeBV <jc.lee@birdview.kr>
2022-12-27 15:12:37 +0900

    3.1 Login Form Part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          onChange={handleChange}
+          value={password}
+        />
+        <input type="submit" value="Login" />
+      </form>
+
+      <div>
+        <button>Continue With Google</button>
+        <button>Continue With Github</button>
+      </div>
+    </div>
+  );
+};
+
 export default Auth;
