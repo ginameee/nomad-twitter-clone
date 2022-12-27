47ab3d1511b3fc28fdca6293b8150755c294edce
ginameeeBV <jc.lee@birdview.kr>
2022-12-27 12:57:13 +0900

    2.0 Setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppRouter from "./components/AppRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<AppRouter></AppRouter>);
